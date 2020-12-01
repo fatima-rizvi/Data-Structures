a2e388e890dcbcaeda638bc0f7e794c895af4772
Fatima Rizvi <fatimarzv23@gmail.com>
2020-12-01 10:25:17 -0600

    In progress

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,7 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+        
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +28,19 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        old_head = self.head
+        new_node.next = old_head #You only need new_node.next = self.head but this explains what's going on better
+        
+        if self.head is None: #Since there was nothing in the list before this, this becomes the entire thing
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+            return
+
+        old_head.prev = new_node # self.head.prev = new_node (same thing)
+        self.head = new_node #Moves our pointer (head) to the "front". Head is just a variable we use for our computer to point to where we want to start.
+        self.length += 1 #increase length
         
     """
     Removes the List's current head node, making the
@@ -35,7 +48,23 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        if self.length == 0:
+            return
+
+        value = self.head.value #self.head returns the location of the node, not just the value
+        
+        if self.length == 1:
+            self.head = None #The list is empty now
+            self.tail = None #The list is empty now
+            self.length = 0
+            return value
+
+        new_head = self.head.next
+        self.head = new_head
+        self.head.prev = None #Get rid of the pointer to the old head
+        self.length -= 1 #decrease the length
+
+        return value
             
     """
     Wraps the given value in a ListNode and inserts it 
