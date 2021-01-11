e50a5fc2fafed93adb45c1b8794e3ec70cd733f5
Fatima Rizvi <fatimarzv23@gmail.com>
2021-01-10 22:08:06 -0600

    Finished notes in queue

--- queue/queue.py
+++ queue/queue.py
@@ -4,7 +4,7 @@
 
 import sys
 sys.path.append('../singly_linked_list')
-from singly_linked_list import LinkedList
+from singly_linked_list import LinkedList   # here's a shorter import that we probs could've used for stack.py :P
 
 """
 A queue is a data structure whose primary purpose is to store and
@@ -21,20 +21,24 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# Using out LinkedList class (import)
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        self.storage = LinkedList() # 1. What do we want to set self.storage equal to? 
     
     def __len__(self):
-        return self.size
+        return self.size    # 2. How do we want to get the size of the list? 
 
-    def enqueue(self, value):
-        self.size += 1  # self.size = self.size + 1 (same thing)
-        self.storage.add_to_tail(value)
+    def enqueue(self, value):  # 3 # Means to add it to the queue (at the end, bc it's a queue)
+        self.size += 1  # 5. self.size = self.size + 1 (same thing)
+        self.storage.add_to_tail(value) # 4
 
-    def dequeue(self):
-        if self.size == 0:
+    def dequeue(self):  # 6 # Means to remove it from the queue (from the head, bc it's a queue)
+        if self.size == 0: # 9. Q: Is there an edge case? What is it and what do we do? what if there isn't anything in the list?
             return
-        self.size -= 1 # self.size = self.size - 1 (same thing)
-        return self.storage.remove_head()
+        self.size -= 1 # 8 # self.size = self.size - 1 (same thing)
+        return self.storage.remove_head() # 7 
+
+# We're not going to do this with a regular list for time's sake, but you get the idea
